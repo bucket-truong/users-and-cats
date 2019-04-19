df5267a26ab63ff6252051c0e33eb85a028f12d9
Davis Truong <buckets_truong@Daviss-MacBook-Pro.local>
2019-04-19 16:42:56 -0600

    users and cats

--- controllers/catsController.js
+++ controllers/catsController.js
@@ -4,18 +4,23 @@ const Cat = require('../models/Cat');
 const User = require('../models/User');
 
 router.get('/', (req, res)=>{
+  //find all of the cats from the db
     Cat.find({}, (err, catsFromTheDatabase)=>{
+      //render the index page from cats folder
         res.render('cats/index.ejs', {
+          //send the catsFromTheDatabase under the name
             catsOnTheTemplate: catsFromTheDatabase
         })
     })
 })
-
+//
 router.get('/new', (req, res)=>{
+  //find all users
     User.find({}, (error, allUsers) => {
         if(error) {
             res.send(error)
         } else {
+          //display all the users to the new page
             res.render('cats/new.ejs', {
                 usersOnTemplate: allUsers
             })
@@ -25,10 +30,14 @@ router.get('/new', (req, res)=>{
 })
 
 router.get('/:id', (req, res) => {
+  //finding an id of a cat from the data base
     Cat.findById(req.params.id, (err, catFromTheDatabase) => {
+      //finding the user that that has the the specific cat id
       User.findOne({
+        //setting the a variable for req.params.id
         "cats": req.params.id
       }, (err, user) => {
+        //showing both the, cat and the user in the show page
         res.render('cats/show.ejs', {
           catOnTheTemplate: catFromTheDatabase,
           user: user
@@ -36,10 +45,13 @@ router.get('/:id', (req, res) => {
       })
     })
    })
-
+//editing the users and cats
 router.get('/:id/edit', (req, res)=>{
+  //finding the id of the cat from the data base
     Cat.findById(req.params.id, (err, catFromTheDatabase)=>{
+      //finding the user with no cat from the data base
         User.find({}, (err, usersFromTheDatabase)=>{
+          //displaying the edited users and cats
             res.render('cats/edit.ejs', {
                 catOnTheTemplate: catFromTheDatabase,
                 usersOnTemplate: usersFromTheDatabase
@@ -49,16 +61,22 @@ router.get('/:id/edit', (req, res)=>{
     })
 })
 
-
+//posting to the root
 router.post('/', (req, res)=>{
     console.log(req.body);
+    //creating a cat
     Cat.create(req.body, (err, newlyCreatedCat)=>{
+      //testing to the create route
         console.log(`Created a cat for user ${req.body.userId}`);
+        //finding a user by id
         User.findById(req.body.userId, function(err, userFound)
         {
+          //assigning the created cat to user by
             userFound.cats.push(newlyCreatedCat._id);
+            //saving both the user and the new cat that was assigned
             userFound.save((err, savedUser)=>{
                 console.log(savedUser);
+                //redirecting back to the cats view
                 res.redirect('/cats')
             });
         });
@@ -68,14 +86,23 @@ router.post('/', (req, res)=>{
 
 router.put('/:id', (req, res)=>{
     console.log(req.body);
+    //finding the cat by id and updating
     Cat.findByIdAndUpdate(req.params.id, req.body, {new: true},(err, updatedCat)=>{
+      //finding the user the cat is attatched to
       User.findOne({'cats': req.params.id}, (err, foundUser) => {
+        //taking the found user
         if(foundUser._id.toString() !== req.body.userId){
+          //taking out the current cat that the user owns
           foundUser.cats.remove(req.params.id);
+          //saving the user's current after removing
           foundUser.save((err, savedFoundUser) => {
+            //finding another user by id
             User.findById(req.body.userId, (err, newUser) => {
+              //taking that new user and update the cate to the new users
               newUser.cats.push(updatedCat._id);
+              //saving the new user and updated cat
               newUser.save((err, savedNewUser) => {
+                //redirecting to the cats view
                 res.redirect('/cats/' + req.params.id);
               })
             })
@@ -88,23 +115,23 @@ router.put('/:id', (req, res)=>{
   });
 
 router.delete('/:id', (req, res)=>{
+  //finding a cat by id from the data base
     Cat.findByIdAndDelete(req.params.id, (err, catFromTheDatabase)=>{
+      //checking to see if the cat was found
         console.log(catFromTheDatabase);
-        res.redirect('/cats');
-    })
-})
-router.delete('/:id', (req, res)=>{
-    Cat.findByIdAndDelete(req.params.id, (err, catFromTheDatabase)=>{
-        console.log(catFromTheDatabase);
+        //finding the user from the cat id
         User.findOne({'cats': req.params.id}, (err, foundUser)=>{
             if(err){
                 console.log(err)
             }else{
                 console.log(foundUser);
+                //removing the cat from the user that was found
                 foundUser.cats.remove(req.params.id);
+                //saving the updated user
                 foundUser.save((err, updatedUser)=>{
                     console.log(updatedUser);
-                    res.redirect('/cats');  
+                    //redirect to the cat view
+                    res.redirect('/cats');
                 })
             };
         });
--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,9 @@ const User = require('../models/User');
 const Cat = require('../models/Cat');
 
 router.get('/', (req, res)=>{
+  //finding all user from the database
     User.find({}, (err, usersFromTheDatabase)=>{
+      //displaying all the users to the index
         res.render('users/index.ejs', {
             usersOnTheTemplate: usersFromTheDatabase
         })
@@ -16,12 +18,16 @@ router.get('/new', (req, res)=>{
 })
 
 router.get('/:id', (req, res)=>{
-    User.findById(req.params.id) 
+  //finding a user by id
+    User.findById(req.params.id)
+    //populating the cats path
     .populate('cats')
+    //executes
     .exec((err, userFromTheDatabase)=>{
         if(err){
             res.send(err);
         } else {
+          //displays to the users show page
             res.render('users/show.ejs', {
                 userOnTheTemplate: userFromTheDatabase
             });
@@ -31,7 +37,9 @@ router.get('/:id', (req, res)=>{
 })
 
 router.get('/:id/edit', (req, res)=>{
+  //finding a user by id
     User.findById(req.params.id, (err, userFromTheDatabase)=>{
+      //displays the edited user to the user edit page
         res.render('users/edit.ejs', {
             userOnTheTemplate: userFromTheDatabase
         })
@@ -39,28 +47,35 @@ router.get('/:id/edit', (req, res)=>{
 })
 
 router.post('/', (req, res)=>{
+  //creating a new user
     User.create(req.body, (err, newlyCreatedUser)=>{
         console.log(newlyCreatedUser)
+        //redirecting to the user path
         res.redirect('/users')
     })
 })
 
 router.put('/:id', (req, res)=>{
+  //find a user by id from db and updating
     User.findByIdAndUpdate(req.params.id, req.body, (err, userFromTheDatabase)=>{
         console.log(userFromTheDatabase);
+        //redirecting to to the users path
         res.redirect('/users');
     })
 })
 
 router.delete('/:id', (req, res)=>{
+  //finding a user by id and deleting user
     User.findByIdAndDelete(req.params.id, (err, userFromTheDatabase)=>{
         console.log(userFromTheDatabase);
+        //deleting the cat assigned to user
         Cat.deleteMany({
             _id: {
                 $in: userFromTheDatabase.cats
             }
         }, (err, data)=>{
             console.log(data);
+            //redirecting to users path
             res.redirect('/users');
         })
     })
